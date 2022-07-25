d23f81f1735ff86552126c32848611ee5688eeee
Bung Danar <danar.g.putera@gmail.com>
2022-07-25 08:52:48 +0700

    chore: End of Section 6

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,20 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+  await client.hSet('car', {
+    color: 'red',
+    year: 1950
+  });
+
+  const car = await client.hGetAll('car#123');
+
+  if (!Object.keys(car).length) {
+    console.log('Car not found, respond with 404');
+    return;
+  }
+
+  console.log(car);
+};
+
 run();
